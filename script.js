f2bb23356aa0c306b50a696024ab3d0f3cf45e2a
victorhugo98 <victor.matos98@hotmail.com>
2022-07-04 12:36:36 -0300

    adc javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+const linksInternos = document.querySelectorAll('a[href^="#"]')
+function scrollSuave(){
+linksInternos.forEach(item =>
+  {
+    item.addEventListener('click', (e)=>
+   {  e.preventDefault()
+        const href = e.target.getAttribute('href')
+        const buscarId = document.querySelector(href)
+          window.scroll({
+            top:buscarId.offsetTop,
+            behavior: "smooth",
+          })
+  } ) })
+}
+scrollSuave()
+
+const secoes = document.querySelectorAll('.js-animacao')
+secoes[0].classList.add('ativo')
+function animarItens(){
+secoes.forEach((item) =>
+{ const metadeTela = window.innerWidth * 0.5
+  const topItem = item.getBoundingClientRect().top - metadeTela;
+  if(topItem < 0 )
+  item.classList.add('ativo')
+})
+}
+  window.addEventListener('scroll', animarItens)
